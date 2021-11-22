9520decbc9bb11cba5f707aaf81af1e5fd52363a
fenota-viktoriya <Fen9206@gmail.com>
2021-11-22 06:15:27 +0200

    футер

--- js/modal.js
+++ js/modal.js
@@ -13,6 +13,8 @@
     }
   
 })();
+
+
 (() => {
   const mobileMenu = document.querySelector('[data-menu]');
   const openMenuBtn = document.querySelector('[data-menu-open]');
@@ -31,4 +33,14 @@
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);})();
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 768px)').addEventListener('change', (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
